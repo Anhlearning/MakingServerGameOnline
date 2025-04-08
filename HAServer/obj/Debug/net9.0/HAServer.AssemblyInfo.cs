1d9d30584ce3408c6db681130179a8a4d5329d7a
Dinh Hoang Anh <110513024+Anhlearning@users.noreply.github.com>
2025-04-08 22:58:42 +0700

    Config main

--- HAServer/obj/Debug/net9.0/HAServer.AssemblyInfo.cs
+++ HAServer/obj/Debug/net9.0/HAServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HAServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+591ae6b7b003213197edfce97709587055abce0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("HAServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HAServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
